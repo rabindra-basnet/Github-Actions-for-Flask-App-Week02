335ff29f255d19032cb704dd2db96b33d3d43774
Rabindra Basnet <rabindrabasnet82@gmail.com>
2025-11-09 17:22:29 +0000

    added a tag

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "Hello From CI/CD pipeline with github Actions!"
+    return "Hello From CI/CD pipeline with github Actions! and Presidential"
 
 if __name__ == "__main__":
     app.run("0.0.0.0",debug=True)
