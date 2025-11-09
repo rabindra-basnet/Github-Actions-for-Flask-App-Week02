5888466f88eae3271fbf02dafc4896246405074a
Ubuntu <ubuntu@ip-172-31-21-137.eu-north-1.compute.internal>
2025-11-09 15:48:36 +0000

    flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "Hello From CI/CD pipeline with github Actions!"
+
+if __name__ == "__main__":
+    app.run("0.0.0.0",debug=True)
